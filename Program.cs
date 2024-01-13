f1c7fc968186aa6e0c244ac07f28ec1d25538aac
Luigi W Luz <luigiluz22@gmail.com>
2024-01-13 14:35:33 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.Write ("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1234",modelo: "nokia123",imei: "0800",memoria: 128);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Minecraft");
+
+
+Console.WriteLine("/n");
+
+
+Console.Write ("Smartphone Nokia: ");
+Smartphone iphone = new Iphone(numero: "7889",modelo: "Iponei",imei: "4563",memoria: 256);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
